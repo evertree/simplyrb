87414a1927534e050eba9f8ae84f7b4a4c834efa
Billy Morales <evertree@gmail.com>
2012-10-22 16:13:31 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Simplyrb::Application.config.secret_token = '6ee1fa78cfb32f19327f6c129a13ec8b180c06aab3dbceda419b3607a236b05f8095ee0b0c178ab3d328487895e921f7b567bb855ca0b0be84410b2ebe929bfd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Simplyrb::Application.config.session_store :cookie_store, key: '_simplyrb_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Simplyrb::Application.config.session_store :active_record_store
