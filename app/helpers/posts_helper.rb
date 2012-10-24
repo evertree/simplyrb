ff4439f1e8784508c087792dae66a28205eb4423
Billy Morales <evertree@gmail.com>
2012-10-24 01:37:50 -0700

    Fix bug in comment_helper

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,6 +1,6 @@
 module PostsHelper
 	def comment_count
 		phrase = @post.comments.count.to_s
-		phrase += phrase == '1'? " comments" : " comments"
+		phrase += phrase == '1'? " comment" : " comments"
 	end
 end
