2ec022c16a08c837e6de44a981d9afffcadea285
Billy Morales <evertree@gmail.com>
2012-10-24 02:18:23 -0700

    Migrate for production

--- db/schema.rb
+++ db/schema.rb
@@ -14,11 +14,11 @@
 ActiveRecord::Schema.define(:version => 20121023034120) do
 
   create_table "comments", :force => true do |t|
-    t.string   "commenter"
+    t.string   "commenter",  :default => "Anonymous"
     t.text     "body"
     t.integer  "post_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",                          :null => false
+    t.datetime "updated_at",                          :null => false
   end
 
   create_table "posts", :force => true do |t|
