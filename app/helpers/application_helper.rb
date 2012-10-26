5af760553c2f45cd13d71913012271a4c0d83352
Billy Morales <evertree@gmail.com>
2012-10-26 01:03:17 -0700

    Minor CSS tweaks

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,9 @@ module ApplicationHelper
 		time = time_ago_in_words(date).gsub(/about /,"")
 		time += " ago"
 	end
+
+	def p_strip(text)
+		text = text.gsub(/\A<p>/,"")
+		text = sanitize(text.gsub(/<\/p>\z/,""))
+	end
 end
